b5a12786e76036fd9557c2c9b134d644e7812d19
theAleXX <th3alexx@gmail.com>
2026-08-11 11:25:10 +0200

    add Prisma client

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { StagesModule } from './stages/stages.module';
 import { CompetitionsModule } from './competitions/competitions.module';
 import { DatabaseModule } from './database/database.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [StagesModule, CompetitionsModule, DatabaseModule],
+  imports: [StagesModule, CompetitionsModule, DatabaseModule, PrismaModule],
 })
 export class AppModule {}
